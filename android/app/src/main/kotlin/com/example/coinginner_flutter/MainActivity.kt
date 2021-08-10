e6780ad59d9dce25185e4ac8929cbc1287f9fdb0
congnguyendinh0 <congnguyendinh0@gmail.com>
2021-08-10 17:32:54 +0200

    setup firebase

---
+++ android/app/src/main/kotlin/com/example/coinginner_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coinginner_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/glossary_screen.dart
@@ -0,0 +1,2 @@
+// followed this tutorial by https://www.youtube.com/watch?v=WuYOGBEOEOo&ab_channel=LearnFlutterwithMe //
+import 'package:flutter/material.dart';
